2e3474e746c17fc6bde01cbefaf3cfa94e8f5904
User <s0k_kid@yahoo.com>
2025-04-17 15:15:30 +0700

    fix: restore original format of index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import ProductExample from './components/ProductExample';
-import { icon10CSData } from './data';
+import icon10CSData from './data/icon10CSData';
 import './styles/global.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-
-// Chuyển đổi giữa true/false để xem ví dụ hoặc trang sản phẩm đầy đủ
-const showExampleComponent = true;
-
 root.render(
   <React.StrictMode>
-    {showExampleComponent ? (
-      <ProductExample />
-    ) : (
-      <App productData={icon10CSData.productInfo} />
-    )}
+    <App productData={icon10CSData.productInfo} />
   </React.StrictMode>
 );
