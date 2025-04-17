60705da7fa0bc49e188c426554e38b1bb0a5f09a
User <s0k_kid@yahoo.com>
2025-04-17 14:58:44 +0700

    feat: update index.tsx to use Icon 10CS data and example component

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import ProductExample from './components/ProductExample';
+import { icon10CSData } from './data';
 import './styles/global.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+// Toggle between true/false to switch between ProductExample and App
+const showExampleComponent = true;
+
 root.render(
   <React.StrictMode>
-    <App />
+    {showExampleComponent ? (
+      <ProductExample />
+    ) : (
+      <App productData={icon10CSData.productInfo} />
+    )}
   </React.StrictMode>
 );
